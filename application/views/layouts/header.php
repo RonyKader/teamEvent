13fe3ee6b5ed2751533b348f5795151bd1916779
RonyKader <rony.ksr06@gmial.com>
2016-08-09 14:51:49 +0100

    header modification

--- application/views/layouts/header.php
+++ application/views/layouts/header.php
@@ -78,7 +78,7 @@
                                             <li><a href="homepage2.html">Homepage Two</a></li>
                                         </ul> -->
                                     </li>
-                                    <li class="active"><a href="<?php echo base_url();?>home/buytickets/#buytickets">Buy Tickets</a></li>
+                                    <li class="active"><a href="<?php echo base_url();?>home/buytickets">Buy Tickets</a></li>
                                     <li class="dropdown menu-item menu-item-type-taxonomy menu-item-object-category menu-item-has-children menu-item-376">
                                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Pages</a>
                                         <ul class="dropdown-menu sub-menu menu-sub-content">
@@ -105,7 +105,7 @@
                                             <li><a href="blog-single.html">Blog Single</a></li>
                                         </ul>
                                     </li>
-                                    <li><a href="<?php echo base_url();?>auth/registration/#reg">Login/Registration</a></li>
+                                    <li><a href="<?php echo base_url();?>auth/registration">Login/Registration</a></li>
                                 </ul>
                                 <div class="menu-shopping-cart">
                                     <div class="cart-icon">
@@ -158,53 +158,4 @@
         </nav>
     </header>
     <!-- end header  -->
-        <!--  bannar section start  -->
-    <section class="bannar">
-        <div class="bannar-img">
-            <img src="<?php echo base_url();?>/assets/images/homepage1/bannar-bg.jpg" alt="Banner">
-            <div class="overlay"></div>
-            <div class="bannar-conent-area">
-                <div class="bannar-header">
-                    <div class="container">
-                        <div class="row">
-                            <h1>Design Conference 2016</h1>
-                            <ul class="meta-post">
-                                <li>
-                                    <i class="fa fa-calendar"></i>
-                                    22 February, 2016
-                                </li>
-                                <li>
-                                    <i class="fa fa-map-marker"></i>
-                                    Lorng Prime Garder, USA
-                                </li>
-                            </ul>
-                        </div><!-- /.row -->
-                    </div><!-- /.container -->
-                </div><!-- end bannar header  -->
-
-                <div class="bannar-content">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-sm-6 col-md-8">
-                                
-                            <div class="header-countdown-wrapper">
-                                <div class="header-countdown">
-                                    <div id="header-countdown" data-days="days" data-hrs="hrs" data-min="min" data-sec="sec" data-label="time left" data-gmt="0">2017-07-12 8:00:00</div>
-                                </div>
-                            </div>
-                            </div><!-- /.col-md-8 -->
-
-                            <div class="col-sm-6 col-md-4">
-                                <div class="row">
-                                    <div class="bannar-btn">
-                                        <a href="<?php echo base_url();?>auth/registration/#reg" class="register-now hvr-bounce-to-bottom">Register Now</a>
-                                        <a href="#" class="view-detail hvr-bounce-to-bottom">View Details</a>
-                                    </div>
-                                </div><!-- /.row -->
-                            </div><!-- /.col-md-4 -->
-                        </div><!-- /.row -->
-                    </div><!-- /.container -->
-                </div><!-- /.bannar-content -->
-            </div><!-- /.banner conent area -->
-        </div><!-- /.banner img -->
-    </section>
+       
